0aab3d3bbcb81b9413e5db5819afc5ee3a0c5bf0
Saptarshi Mondal <smzaptarshi@gmail.com>
2024-08-04 22:24:02 +0530

    Update taskManager.ts

--- web/src/modules/taskManager.ts
+++ web/src/modules/taskManager.ts
@@ -1,40 +1,39 @@
-// // src/taskManager.ts
 
-// import Task from "@/model/Task";
-// import { initialTasks } from "@/utils/TaskList";
-
+import Task from "@/model/Task";
+import { initialTasks } from "@/utils/TaskList";
 
 
-// let tasks: Task[] = [...initialTasks];
 
-// export function initializeTasks() {
+let tasks: Task[] = [...initialTasks];
 
-// }
+export function initializeTasks() {
 
-// export function getActiveTasks(): Task[] {
+}
+
+export function getActiveTasks(): Task[] {
  
-// }
+}
 
-// export function getCompletedTasks(): Task[] {
+export function getCompletedTasks(): Task[] {
  
-// }
+}
 
-// export function getAllTasks(): Task[] {
+export function getAllTasks(): Task[] {
  
-// }
+}
 
-// export function completeTask(taskTitle: string): void {
+export function completeTask(taskTitle: string): void {
   
-// }
+}
 
-// export function createTask(title: string, description: string, persona: string, group: number): void {
+export function createTask(title: string, description: string, persona: string, group: number): void {
   
-// }
+}
 
-// export function updateTask(taskId: number, updatedTask: Partial<Omit<Task, 'id'>>): void {
+export function updateTask(taskId: number, updatedTask: Partial<Omit<Task, 'id'>>): void {
  
-// }
+}
 
-// export function deleteTask(taskId: number): void {
+export function deleteTask(taskId: number): void {
  
-// }
+}
